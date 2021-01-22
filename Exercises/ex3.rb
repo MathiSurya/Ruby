b55a23b974548174fc4d44e267f73b0b003be74b
Surya <aguerosurya473@protonmail.ch>
2021-01-22 14:16:02 +0530

    Update ex3.rb

--- Exercises/ex3.rb
+++ Exercises/ex3.rb
@@ -1,12 +1,17 @@
 puts "I will try some arithmetic operations";
 
+# #{} is used to compute things inside a strings, ruby will calculate values instead of printing them directly 
 puts "Hens #{30+35/5}"
 puts "Roosters #{40+30/2}"
 
 puts "Now i will count the eggs:"
+
+#Order of precedence will be starts from multiplication,division,addition and subtraction
 puts 3+4-2*5/6*2%5
 
 puts "Is it true that 3+2-5*5>20?"
+
+#Stands whether the condition is true or false
 puts 3+2-5*5>20
  
 puts "what is 3+3? #{3+3}"
