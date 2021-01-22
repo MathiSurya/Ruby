50b20970cdf48ad3b6934c4349592f587ec19752
Surya <aguerosurya473@protonmail.ch>
2021-01-22 11:10:03 +0530

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!";
+puts "Hi there!";
+puts "Welcome to ruby world!";
+puts "Ruby here!";
+puts "How are you?";
+puts "I'd much rather you 'not'.";
+puts 'I "said" do not touch this';
