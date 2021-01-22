1dfc03d3a25cce33282581fb75047f8ed997d8d0
Surya <aguerosurya473@protonmail.ch>
2021-01-22 14:07:12 +0530

    Add files via upload

---
+++ Exercises/ex3.rb
@@ -0,0 +1,19 @@
+puts "I will try some arithmetic operations";
+
+puts "Hens #{30+35/5}"
+puts "Roosters #{40+30/2}"
+
+puts "Now i will count the eggs:"
+puts 3+4-2*5/6*2%5
+
+puts "Is it true that 3+2-5*5>20?"
+puts 3+2-5*5>20
+ 
+puts "what is 3+3? #{3+3}"
+puts "what is 5-4? #{5-4}"
+
+puts "So that it is false";
+puts "need some more?";
+
+puts "Is it greater? #{5>3}";
+puts "Is it less than or equal? #{5<=8}"
