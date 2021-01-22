b5580ef66d9432b1c626caa4272b4c95092fac3b
Surya <aguerosurya473@protonmail.ch>
2021-01-22 11:43:00 +0530

    Delete ex1.rb

--- ex1.rb
+++
@@ -1,7 +0,0 @@
-puts "Hello World!";
-puts "Hi there!";
-puts "Welcome to ruby world!";
-puts "Ruby here!";
-puts "How are you?";
-puts "I'd much rather you 'not'.";
-puts 'I "said" do not touch this';
