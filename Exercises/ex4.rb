afc583382eeb4386450380cb2532a3e64121c07d
Surya <aguerosurya473@protonmail.ch>
2021-01-23 10:57:58 +0530

    Update ex4.rb

--- Exercises/ex4.rb
+++ Exercises/ex4.rb
@@ -13,3 +13,13 @@ puts "There will be #{cars_not_driven} empty cars today."
 puts "We can transport #{carpool_capacity} people today."
 puts "we have #{passengers} to carpool today."
 puts "we need to put about #{average_passengers_per_car} in each car."
+puts "_________________________________________________________________________________________________________"
+#1)what is undefined local variable error?
+# It is because you haven't created that variable yet.
+
+#2)I used 4.0 for space_in_a_car, but is that necessary? What happens if it's just 4?
+puts "I used 4.0 for space_in_a_car, but is that necessary? What happens if it's just 4?"
+puts "If  any one of the value is in floating point then the result will also be in floating point, otherwise it'll be an integer."
+puts "For Example....."
+puts "If you add 10+10 ,the answer will be #{10+10}";
+puts "But if you add 10+10.0 ,the answer will be #{10+10.0}";
