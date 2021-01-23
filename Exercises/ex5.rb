d072bd6a5c69496fb4ed80b5055ec1619e6f2da7
Surya <aguerosurya473@protonmail.ch>
2021-01-23 12:14:18 +0530

    Add files via upload

---
+++ Exercises/ex5.rb
@@ -0,0 +1,25 @@
+my_name = 'Surya';
+my_age = 21
+my_height = 170 #cms
+my_eyes = "Black"
+my_weight = 57 #kgs
+my_teeth = "white"
+my_hair = "Black"
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} cms tall."
+puts "He's #{my_weight} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age},#{my_height} and #{my_weight} I get #{my_weight + my_height + my_age}."
+puts "_______________________________________________________________________"
+
+puts "Now we'll get the values in inches and pounds"
+puts "One inch equals to 2.54 cms"
+puts "One kg equals to 2.20462 pounds"
+my_height_inches = 170 / 2.54
+my_weight_pounds = 57 * 2.20462
+puts "So, he's #{my_height_inches} inches tall"
+puts "So, he's #{my_weight_pounds} pounds heavy"
