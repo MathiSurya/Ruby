ead2b8f8bf37b21e9cf3d2bdd13d920ade6df13a
Surya <aguerosurya473@protonmail.ch>
2021-01-27 22:33:47 +0530

    Update exe11.rb

--- Exercises/exe11.rb
+++ Exercises/exe11.rb
@@ -1,8 +1,21 @@
 print "How old are you? "
+#gets is a method used to receive user's input and chomp is used to cut the new line being created
 age = gets.chomp
 print "How tall you are? "
-height = gets.chop
+height = gets.chomp
 print "How much do you weigh? "
 weight = gets.chomp
+
+puts "So, you're #{age} old,#{height} tall and #{weight} heavy."
+
+
+#for converting the user input into integers we use gets.chomp.to_i
+
+print "How old are you? "
+age = gets.chomp.to_i
+print "How tall you are? "
+height = gets.chomp.to_i
+print "How much do you weigh? "
+weight = gets.chomp.to_i
 
 puts "So, you're #{age} old,#{height} tall and #{weight} heavy."
