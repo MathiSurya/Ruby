659d30b8978efe6c5068d9f8dcb5d416eaa4a1a9
Surya <aguerosurya473@protonmail.ch>
2021-01-27 23:26:09 +0530

    Update exe12.rb

--- Exercises/exe12.rb
+++ Exercises/exe12.rb
@@ -1,4 +1,5 @@
 print "Give me a number: "
+#getting user input and converting the number into an integer
 number = gets.chomp.to_i
 
 bigger = number * 100
@@ -13,7 +14,9 @@ puts "A smaller number is #{smaller}."
 
 #To play with .to_f more, make a small script that asks for some money and gives back 10% of it. If I give your script 103.4 (dollars), your script gives me back 10.34 in change
 print "Give me a some money: "
+#getting the input from user and converting the number into floating point
 money = gets.chomp.to_f
+#dividing the value by 10,so that we'll have 10% in return to our hand
 return_money = money / 10
 puts "So you will have #{return_money} in hand."
 
