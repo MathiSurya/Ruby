859a8fb15d86d05a06ca81add182aaaf3994d6bc
Surya <aguerosurya473@protonmail.ch>
2021-01-27 11:48:32 +0530

    Update ex9.rb

--- Exercises/ex9.rb
+++ Exercises/ex9.rb
@@ -1,9 +1,11 @@
 days = "Mon Tue Wed Thu Fri Sat Sun"
+#\n is used to get a new line 
 months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
-
+#printing days
 puts "Here are the days:#{days}"
+#printing months with new lines everytime
 puts "Here are the months:#{months}"
-
+#%q is used to create strings without using quotation marks
 puts %q{
 	There's something going on here.
 	with this weired quote
