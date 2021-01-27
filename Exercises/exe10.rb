b72b01be2739f02ee13a31edef33fe8b27864048
Surya <aguerosurya473@protonmail.ch>
2021-01-27 13:28:24 +0530

    Update exe10.rb

--- Exercises/exe10.rb
+++ Exercises/exe10.rb
@@ -1,14 +1,18 @@
+#\t will make a horizontal tab space
 tabby_cat = "\tI'm tabbed in."
+#\n is for creating a new line
 persian_cat = "I'm split on\na new line."
+#print a backslash
 backslash_cat = "I'm \\ a \\ cat."
 
-fat_cat = '''
+#If we use double quotes escape sequences are executed
+#But if we use single quotes strings within the quotes are printed as it was given
+fat_cat = """
 I\'ll do a list:
 \t* cat food 
 \t* fishies
 \t* catnip\n\t* Grass
-'''
-
+""" 
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
