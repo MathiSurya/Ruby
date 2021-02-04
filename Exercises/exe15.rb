ad675232cd78f874986b962010673b4fc8618065
Surya <aguerosurya473@protonmail.ch>
2021-02-04 10:35:50 +0530

    Update exe15.rb

--- Exercises/exe15.rb
+++ Exercises/exe15.rb
@@ -1,12 +1,15 @@
+#uses ARGV to get a filename
 filename = ARGV.first
-
+#here we open the above file
 txt = open(filename)
-
+#printing filename
 puts "Here's your file #{filename}:"
+#here we use the read function to read the opened file.
 puts txt.read
-
+#printing filenaee again
 print "Type the filename again:"
+#getting the file again using standard input function
 file_again = $stdin.gets.chomp
-
+#reading the file again
 txt_again = open(file_again)
 print txt_again.read
